30ef8e68e0c624e54451ce1bdeb9cf2155eb7e41
Erick.Diaz <erick.diaz@cygnus.cl>
2023-03-08 17:30:06 -0300

    se mejora pagina de inicio

--- Login-FRONT/wwwroot/js/cygnus-base.js
+++ Login-FRONT/wwwroot/js/cygnus-base.js
@@ -37,7 +37,6 @@ function LoginCygnus(url, data) {
         .catch((error) => console.log(error))
 }
 
-
 function login() {
 
     var user = document.getElementById('txtUsuario').value;
@@ -58,118 +57,6 @@ function login() {
     LoginCygnus(url, data);
 
 }
-
-//function login_E() {
-//    var sistemas = []
-//    var user = document.getElementById('txtUsuario').value;
-//    var pass = document.getElementById('txtPass').value;
-//    var url = host() + 'Usuario/Usuario_Traer_Sistemas';
-
-//    var myHeaders = new Headers();
-//    myHeaders.append("Content-Type", "application/json");
-
-//    var raw = JSON.stringify({
-//        "USR_LOGIN": user,
-//        "USR_PASSWORD": pass
-//    });
-
-//    var requestOptions = {
-//        method: 'POST',
-//        headers: myHeaders,
-//        body: raw,
-//        redirect: 'follow'
-//    };
-
-//    fetch(url, requestOptions)
-//        .then(response => response.json())
-//        .then(result => {
-//            console.log(result);
-//            result = sistemas;
-//            if (error instanceof Error && error.message.includes("400")) {
-//                document.getElementById("alertPass_2").style.display = "block";
-//                document.getElementById("msjeAcceso").innerHTML = "Error en sus credenciales, verifiquelas y vuelva a intentarlo";
-//            } else {
-//                window.location.href = 'https://localhost:44304/InicioB/Index';
-//            }
-//        })
-//        .catch(error => console.log('error', error));
-
-
-//        /*window.location.href = 'https://localhost:44304/InicioB/Index';*/
-
-//}
-
-//.then(result => {
-        //    /*console.log(result);*/
-        //    var parsedResult = JSON.parse(result);
-        //    var uxS_AUTOID = parsedResult[0].uxS_AUTOID;
-        //    var siS_AUTOID = parsedResult[0].siS_AUTOID;
-        //    var siS_DESCRIPCION = parsedResult[0].siS_DESCRIPCION;
-        //    var siS_URL = parsedResult[0].siS_URL;
-        //    var siS_TOOLTIP = parsedResult[0].siS_TOOLTIP;
-        //    var siS_ICON_URL = parsedResult[0].siS_ICON_URL;
-        //    window.location.href = 'https://localhost:44304/InicioB/Index';
-        //    console.log(uxS_AUTOID);
-        //    console.log(siS_AUTOID);
-        //    console.log(siS_URL);
-        //    console.log(siS_ICON_URL);
-        //    console.log(siS_DESCRIPCION);
-        //    console.log(siS_TOOLTIP);
-
-        //})
-
-//function login_E() {
-//    var urlInicio = 'https://localhost:44304/InicioB/Index';
-//    var sistemas = []
-//    var user = document.getElementById('txtUsuario').value;
-//    var pass = document.getElementById('txtPass').value;
-//    var url = host() + 'Usuario/Usuario_Traer_Sistemas';
-
-//    var myHeaders = new Headers();
-//    myHeaders.append("Content-Type", "application/json");
-
-//    var raw = JSON.stringify({
-//        "USR_LOGIN": user,
-//        "USR_PASSWORD": pass
-//    });
-
-//    var requestOptions = {
-//        method: 'POST',
-//        headers: myHeaders,
-//        body: raw,
-//        redirect: 'follow'
-//    };
-
-//    fetch(url, requestOptions)
-//        .then(response => {
-//            if (!response.ok) {
-//                throw new Error("Bad request");
-//                document.getElementById("alertPass_2").style.display = "block";
-//                document.getElementById("msjeAcceso").innerHTML = "Verifique sus credenciales y vuelva a intentarlo";
-//            }
-//            return response.json();
-//        })
-//        .then(result => {
-//            console.log(result);
-//            sistemas = result;  
-//            console.log('sistemas', sistemas);
-//            console.log("Datos del arreglo sistemas:");
-//            for (let i = 0; i < sistemas.length; i++) {
-//                console.log("Elemento " + i + ":");
-//                console.log(sistemas[i]);
-//            }
-//            /*llenarMenu(sistemas);*/
-//            window.location.href = urlInicio;
-            
-//        })
-
-//        .catch(error => {
-//            console.log('error', error);
-//            document.getElementById("alertPass_2").style.display = "block";
-//            document.getElementById("msjeAcceso").innerHTML = "Verifique sus credenciales y vuelva a intentarlo";
-//        });
-
-//}
 
 function llenarMenu() {
     var url = host() + 'Usuario/Usuario_Traer_Sistemas';
@@ -198,95 +85,121 @@ function llenarMenu() {
             if (!response.ok) {
             }
             return response.json();
-           
+
         })
         .then(result => {
             /*console.log(result);*/
             sistemas = result;
 
-            const container = document.querySelector('.col-md-9');
+                const container = document.querySelector('.col-md-9');
+                const imagenDefault = '/Content/images/gear.png';
+                const exitImagen = '/Content/images/exit-icon.gif';
 
-            for (let i = 0; i < sistemas.length; i += 3) {
+                for (let i = 0; i < sistemas.length; i += 3) {
 
-                const row = document.createElement('div');
-                row.classList.add('row', 'paddingTop');
+                    const row = document.createElement('div');
+                    row.classList.add('row', 'paddingTop');
 
-                const colors = ['btn-secondary', 'btn-success', 'btn-info'];
+                    const colors = ['btn-secondary', 'btn-success', 'btn-info'];
 
-                for (let j = i; j < i + 3 && j < sistemas.length; j++) {
-                    const sistema = sistemas[j];
+                    for (let j = i; j < i + 3 && j < sistemas.length; j++) {
+                        const sistema = sistemas[j];
 
-                    const col = document.createElement('div');
-                    col.classList.add('col-md-4');
+                        const col = document.createElement('div');
+                        col.classList.add('col-md-4');
 
-                    const btn = document.createElement('button');
-                    btn.type = 'button';
-                    btn.classList.add('btn', colors[j % colors.length], 'btn-block');
-                    btn.dataset.url = sistema.siS_URL;
+                        const btn = document.createElement('button');
+                        btn.type = 'button';
+                        btn.classList.add('btn', colors[j % colors.length], 'btn-block');
+                        btn.dataset.url = sistema.siS_URL;
 
-                    const img = document.createElement('img');
-                    img.src = sistema.siS_ICON_URL;
-                    img.alt = sistema.siS_DESCRIPCION;
-                    img.width = 16;
-                    img.height = 16;
+                        const img = document.createElement('img');
+                        //img.src = sistema.siS_ICON_URL;
+                        //img.alt = sistema.siS_DESCRIPCION;
+                        //img.width = 16;
+                        //img.height = 16;
+                        if (sistema.siS_ICON_URL) {
+                            img.src = sistema.siS_ICON_URL;
+                            img.alt = sistema.siS_DESCRIPCION;
+                            img.width = 16;
+                            img.height = 16;
+                        } else {
+                            img.src = imagenDefault;
+                            img.alt = 'Imagen';
+                            img.width = 16;
+                            img.height = 16;
+                        }
+                        //} else {
+                        //    img.style.display = 'none';
+                        //}
+                    
+                        const span = document.createElement('span');
+                        span.textContent = ' ';
 
-                    const span = document.createElement('span');
-                    span.textContent = ' ';
+                        const text = document.createTextNode(sistema.siS_DESCRIPCION);
 
-                    const text = document.createTextNode(sistema.siS_DESCRIPCION);
+                        btn.appendChild(img);
+                        btn.appendChild(span);
+                        btn.appendChild(text);
+                        col.appendChild(btn);
+                        row.appendChild(col);
 
-                    btn.appendChild(img);
-                    btn.appendChild(span);
-                    btn.appendChild(text);
-                    col.appendChild(btn);
-                    row.appendChild(col);
+                        btn.addEventListener('click', function () {
+                            window.open(this.dataset.url, '_blank');
+                        });
+                    }
 
-                    btn.addEventListener('click', function () {
-                        window.open(this.dataset.url, '_blank');
-                    });
+
+                    container.appendChild(row);
                 }
 
+                const lastRow = document.createElement('div');
+                lastRow.classList.add('row', 'paddingTop');
 
-                container.appendChild(row);
-            }
+                const lastCol = document.createElement('div');
+                lastCol.classList.add('col-md-4');
 
-            const lastRow = document.createElement('div');
-            lastRow.classList.add('row', 'paddingTop');
+                const btnCerrar = document.createElement('button');
+                btnCerrar.type = 'button';
+                btnCerrar.classList.add('btn', 'btn-danger', 'btn-block');
 
-            const lastCol = document.createElement('div');
-            lastCol.classList.add('col-md-4');
-
-            const btnCerrar = document.createElement('button');
-            btnCerrar.type = 'button';
-            btnCerrar.classList.add('btn', 'btn-danger', 'btn-block');
+                const imgCerrar = document.createElement('img');
+                imgCerrar.src = exitImagen;
+                imgCerrar.alt = 'Cerrar sesión';
+                imgCerrar.width = 16;
+                imgCerrar.height = 16;
 
             const textCerrar = document.createTextNode('Cerrar sesión');
+            const spanCerrar = document.createElement('span');
+            spanCerrar.textContent = ' ';
 
-            btnCerrar.appendChild(textCerrar);
-            lastCol.appendChild(btnCerrar);
-            lastRow.appendChild(lastCol);
-            container.appendChild(lastRow);
+                btnCerrar.appendChild(imgCerrar);
+                btnCerrar.appendChild(spanCerrar);
+                btnCerrar.appendChild(textCerrar);
+                lastCol.appendChild(btnCerrar);
+                lastRow.appendChild(lastCol);
+                container.appendChild(lastRow);
 
-            btnCerrar.addEventListener('click', function () {
-                window.location.href = urlO;
-                document.getElementById('rut').value = '';
-                document.getElementById('usr').value = '';
-                document.getElementById('uxs').value = '';
-                document.getElementById('ulegajo').value = '';
-                document.getElementById('nombre').value = '';
-                document.getElementById('token').value = '';
-                document.getElementById('ucta').value = '';
+                btnCerrar.addEventListener('click', function () {
+                    window.location.href = urlO;
+                    document.getElementById('rut').value = '';
+                    document.getElementById('usr').value = '';
+                    document.getElementById('uxs').value = '';
+                    document.getElementById('ulegajo').value = '';
+                    document.getElementById('nombre').value = '';
+                    document.getElementById('token').value = '';
+                    document.getElementById('ucta').value = '';
+                });
+
+                /*console.log('sistemas', sistemas);*/
+
+            })
+
+            .catch(error => {
+                console.log('error', error);
             });
 
-            /*console.log('sistemas', sistemas);*/
 
-        })
-
-        .catch(error => {
-            console.log('error', error);  
-        });
-
-    
 
 }
 
@@ -322,7 +235,6 @@ function crearVersiones() {
 
             var ultimasActualizacionesDiv = document.getElementById("ultimasVersiones");
 
-
             ultimasActualizacionesDiv.innerHTML = "";
 
             var titulo = document.createElement("h5");
@@ -332,9 +244,9 @@ function crearVersiones() {
             titulo.style.fontWeight = "bold";
             ultimasActualizacionesDiv.appendChild(titulo);
 
-            for (var i = 0; i < versiones.length; i++)
-            /*for (var i = 0; i < versiones.length && i < 3; i++)*/{
-            var version = versiones[i];
+            /*for (var i = 0; i < versiones.length; i++)*/
+            for (var i = 0; i < versiones.length && i < 3; i++) {
+                var version = versiones[i];
 
 
                 var card = document.createElement("div");
@@ -350,10 +262,12 @@ function crearVersiones() {
                 cardTitle.className = "card-title";
                 cardTitle.style = "text-align: center;";
 
+                var fecha = version.ver_fecha.substring(0, 10);
+
                 var cardSubtitle = document.createElement("h6");
                 cardSubtitle.className = "card-subtitle";
                 cardSubtitle.style = "text-align: center;";
-                cardSubtitle.innerHTML = version.sis_descripcion + "<br>" + version.ver_version + " - Disponible desde: " + version.ver_fecha;
+                cardSubtitle.innerHTML = version.sis_descripcion + "<br>" + version.ver_version + " - Disponible desde : " + fecha;
                 cardSubtitle.style.marginBottom = "0";
 
 
@@ -361,9 +275,9 @@ function crearVersiones() {
                 cardText.className = "card-text text-center";
                 cardText.innerHTML = version.verdet_detalle_version;
                 cardText.style.marginTop = "0";
-                cardText.style.position = "relative";
+                /*cardText.style.position = "relative";*/
                 cardText.style.position = "auto";
-                cardText.style.bottom = "0";
+                //cardText.style.bottom = "0";
 
 
                 cardHeader.appendChild(cardTitle);
@@ -372,11 +286,7 @@ function crearVersiones() {
                 card.appendChild(cardHeader);
                 ultimasActualizacionesDiv.appendChild(card);
 
-                //card.addEventListener("click", function () {
-                //    if (cardText.style.height === "auto") {
-                //        cardText.style.height = "3em"; 
-                //    } 
-                //});
+
             }
 
             /*console.log('versiones', versiones)*/
@@ -386,8 +296,98 @@ function crearVersiones() {
 
 }
 
+function versionesModal() {
+
+    var url = host() + 'Sistema/TraerVersiones';
+    var versiones = [];
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "sis_autoid": "0"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+            }
+            return response.json();
+
+        })
+        .then(result => {
+            /*console.log(result);*/
+            versiones = result
+
+            var ultimasActualizacionesDivModal = document.getElementById("ultimasVersionesMod");
 
 
+            ultimasActualizacionesDivModal.innerHTML = "";
+
+            //var titulo = document.createElement("h5");
+            //titulo.className = "card-title";
+            //titulo.style = "text-align: center;";
+            //titulo.textContent = "Últimas actualizaciones";
+            //titulo.style.fontWeight = "bold";
+            //ultimasActualizacionesDivModal.appendChild(titulo);
+
+
+            for (var i = 0; i < versiones.length; i++) {
+                var version = versiones[i];
+
+
+                var card = document.createElement("div");
+                card.className = "cardM";
+                card.style = "background-color:#D5D4D48F;";
+
+
+                var cardHeader = document.createElement("div");
+                cardHeader.className = "card-headerM";
+                cardHeader.style.marginBottom = "0";
+
+                var cardTitle = document.createElement("h5");
+                cardTitle.className = "card-titleM";
+                cardTitle.style = "text-align: center;";
+
+                var fecha = version.ver_fecha.substring(0, 10);
+                var cardSubtitle = document.createElement("h6");
+                cardSubtitle.className = "card-subtitleM";
+                cardSubtitle.style = "text-align: center;";
+                cardSubtitle.innerHTML = version.sis_descripcion + "<br>" + version.ver_version + " - Disponible desde : " + fecha;
+                cardSubtitle.style.marginBottom = "0";
+
+
+                var cardText = document.createElement("p");
+                cardText.className = "card-text text-center";
+                cardText.innerHTML = version.verdet_detalle_version;
+                cardText.style.marginTop = "0";
+                /*cardText.style.position = "relative";*/
+                cardText.style.position = "auto";
+                //cardText.style.bottom = "0";
+
+
+                cardHeader.appendChild(cardTitle);
+                cardHeader.appendChild(cardSubtitle);
+                cardHeader.appendChild(cardText);
+                card.appendChild(cardHeader);
+                ultimasActualizacionesDivModal.appendChild(card);
+
+
+            }
+
+            /*console.log('versiones', versiones)*/
+        })
+
+        .catch(error => console.log('error', error));
+
+}
 
 function ActualizarPass() {
 
@@ -417,7 +417,7 @@ function ActualizarPass() {
         myHeaders.append("Content-Type", "application/json");
 
         var raw = JSON.stringify({
-            "usR_PASS": USR_PASS,
+            "usR_PASSWORD": USR_PASS,
             "usR_RUT": USR_RUT
         });
 
@@ -531,7 +531,6 @@ function enviarRut() {
 
 }
 
-
 function recuperarPass() {
     var minutosRecuperacionPass = document.getElementById('txtminutosRecuperacionPass').value;
     var USR_RUT = document.getElementById('txtRut').value;
@@ -640,9 +639,6 @@ function recuperarPass() {
 
 }
 
-
-
-/*Limpiar Modal*/
 const myModal = document.getElementById("Modal");
 function limpiarModal() {
     /*console.log('Limpiando modal...');*/
